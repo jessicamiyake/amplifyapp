1b85b025b4872446784b5d5c4ce809f73aa97d79
Jessica Miyake <jmiyake@wisc.edu>
2021-01-31 14:53:28 -0600

    swapped from function to class

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 import {Route, BrowserRouter, Switch} from "react-router-dom";
@@ -5,26 +6,28 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 
-function App() {
-  return (
-    <BrowserRouter>
-    <Switch>
-    <div className="App">
-      <header className="App-header">
-        <a href="/"><img src={logo} className="App-logo" alt="logo" /></a>
-
-        <Route path="/" component={Home} exact />
-        <Route path="/about" component={About} exact />
-        <Route path="/contact" component={Contact} exact />
-        <ul>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-        </header>
-    </div>
-    </Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <Switch>
+      <div className="App">
+        <header className="App-header">
+          <a href="/"><img src={logo} className="App-logo" alt="logo" /></a>
+  
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} exact />
+          <Route path="/contact" component={Contact} exact />
+          <ul>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+          </header>
+      </div>
+      </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
